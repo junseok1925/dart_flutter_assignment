c308c911180e4bd65cea6d295d7f0d435ad7d092
kangJunSeok <rkdwnstjr1925@gmail.com>
2025-09-29 14:07:56 +0900

    feat : Score class, showInfo() method
    점수를 표현하기 위한 Score 클래스, 출력 메서드 생성

--- dart_study/성적 분석/score.dart
+++ dart_study/성적 분석/score.dart
@@ -1,26 +1,15 @@
 class Score {
-  int score = 90;
+  int score;
+  // 생성자
+  Score(this.score);
 
+  // 정보 출력
   void showInfo() {
     print('점수: $score');
   }
 }
 
-class StudentScore extends Score {
-  String name = '강준석';
-  String subject = '수학';
-
-  @override
-  void show() {
-    super.showInfo(); // 부모 클래스의 메서드 호출
-    print('이름 : $name, 과목: $subject');
-  }
-}
-
 void main() {
-  // Score score = Score();
-  // score.showInfo();
-
-  StudentScore studentScore = StudentScore();
-  studentScore.show();
+  Score score = Score(95);
+  score.showInfo();
 }

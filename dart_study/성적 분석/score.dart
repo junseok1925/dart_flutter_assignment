58f33184ddb9b0d2c2370c12ab254dfded508421
kangJunSeok <rkdwnstjr1925@gmail.com>
2025-09-29 12:36:17 +0900

    feat : Score class 상속 받는 StudentScore class 생성
    @overrie 상속 및 부모클래스 메서드 재정의

--- dart_study/성적 분석/score.dart
+++ dart_study/성적 분석/score.dart
@@ -2,12 +2,25 @@ class Score {
   int score = 90;
 
   void showInfo() {
-    Score s = Score();
-    print(s.score);
+    print('점수: $score');
+  }
+}
+
+class StudentScore extends Score {
+  String name = '강준석';
+  String subject = '수학';
+
+  @override
+  void show() {
+    super.showInfo(); // 부모 클래스의 메서드 호출
+    print('이름 : $name, 과목: $subject');
   }
 }
 
 void main() {
-  Score score = Score();
-  score.showInfo();
+  // Score score = Score();
+  // score.showInfo();
+
+  StudentScore studentScore = StudentScore();
+  studentScore.show();
 }
